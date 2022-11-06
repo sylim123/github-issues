9994ef5ecfdb914d25ab15f1dc9aba58fee63b15
sylim123 <sylim123@naver.com>
2022-11-06 22:12:48 +0900

    Revert "[1ST-WEEK-MISSION] 함수로 만드는 Github Issue Page (#1)" (#2)
    This reverts commit 4631289ffb62777d3854ef90b31255eaa8a8df8c.

--- src/api/issue/index.js
+++
@@ -1,9 +0,0 @@
-export const getIssueItemList = async () => {
-  try {
-    const resp = await fetch('data-sources/issues.json');
-    return await resp.json();
-  } catch (e) {
-    console.error(e);
-    return []
-  }
-};
--- src/main.js
+++
@@ -1,8 +0,0 @@
-import App from './App.js';
-import MyReact from './core/MyReact.js';
-
-
-MyReact.render(
-  App,
-  document.getElementById('app'),
-)
