fe7ba3d7a121a7fd4db187e411d39f905e99cfb8
shaun.glass <shaun.glass@kakaocorp.com>
2022-11-10 15:02:11 +0900

    fixed the pipe utils

--- src/utils/pipe.js
+++ src/utils/pipe.js
@@ -1,5 +1,5 @@
 export const pipe = (...functions) =>
-  (...args) => functions.reduce((arg, nextFn) => nextFn(arg), args);
+  args => functions.reduce((arg, nextFn) => nextFn(arg), args);
 
 export const asyncPipe = (...functions) =>
-  (...args) => functions.reduce((promise, nextFn) => promise.then(nextFn), Promise.resolve(args));
+  args => functions.reduce((arg, nextFn) => arg.then(nextFn), Promise.resolve(args));
